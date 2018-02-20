99299f9e42f7994821dde6708933132f104bceb8
Corey Walo <cjwalo@gmail.com>
2018-02-19 21:11:14 -0600

    Initial Commit

---
+++ CWCore/CWCore.h
@@ -0,0 +1,19 @@
+//
+//  CWCore.h
+//  CWCore
+//
+//  Created by Corey Walo on 2/19/18.
+//  Copyright © 2018 Corey Walo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CWCore.
+FOUNDATION_EXPORT double CWCoreVersionNumber;
+
+//! Project version string for CWCore.
+FOUNDATION_EXPORT const unsigned char CWCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CWCore/PublicHeader.h>
+
+
